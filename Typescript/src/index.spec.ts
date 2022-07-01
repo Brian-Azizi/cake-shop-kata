da635f5428729c35982feee9721e90948dd9ece9
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-07-01 11:10:57 +0200

    tests for lead time

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -4,7 +4,24 @@ const calendarDay = (day: WeekDay): Calendar => ({
   day,
 });
 
-test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  const result = order("small", calendarDay("Monday"));
-  expect(result).toStrictEqual(calendarDay("Wednesday"));
+describe("Cake Shop", () => {
+  test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
+    const result = order("small", calendarDay("Monday"));
+    expect(result).toStrictEqual(calendarDay("Wednesday"));
+  });
+
+  test("A small cake, ordered on Tuesday, is delivered on Thursday", () => {
+    const result = order("small", calendarDay("Tuesday"));
+    expect(result).toStrictEqual(calendarDay("Thursday"));
+  });
+
+  test("A big cake, ordered on Monday, is delivered on Thursday", () => {
+    const result = order("big", calendarDay("Monday"));
+    expect(result).toStrictEqual(calendarDay("Thursday"));
+  });
+
+  test("A big cake, ordered on Tuesday, is delivered on Friday", () => {
+    const result = order("big", calendarDay("Tuesday"));
+    expect(result).toStrictEqual(calendarDay("Friday"));
+  });
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,6 +1,13 @@
 type Size = "small" | "big";
 
-export type WeekDay = "Monday" | "Tuesday" | "Wednesday";
+export type WeekDay =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
 
 export interface Calendar {
   day: WeekDay;
@@ -9,6 +16,28 @@ export interface Calendar {
 type OrderDate = Calendar;
 type DeliveryDate = Calendar;
 
+function add(date: Calendar, leadTime: number): Calendar {
+  const DAYS: WeekDay[] = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  function mapToNumber(date: Calendar): number {
+    return DAYS.indexOf(date.day);
+  }
+
+  function mapToDay(number: number): Calendar {
+    return { day: DAYS[number % 7] };
+  }
+
+  return mapToDay(mapToNumber(date) + leadTime);
+}
+
 export function order(size: Size, orderDate: OrderDate): DeliveryDate {
-  return { day: "Wednesday" };
+  const leadTime = size === "small" ? 2 : 3;
+  return add(orderDate, leadTime);
 }
