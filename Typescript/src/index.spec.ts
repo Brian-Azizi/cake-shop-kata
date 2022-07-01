4b1a6607f0cf99dd05f94af90c1bf4b2d08a0fa3
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-07-01 10:55:52 +0200

    First example

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -1,5 +1,10 @@
-import { order } from ".";
+import { Calendar, order, WeekDay } from ".";
+
+const calendarDay = (day: WeekDay): Calendar => ({
+  day,
+});
 
 test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  const result = order("small");
+  const result = order("small", calendarDay("Monday"));
+  expect(result).toStrictEqual(calendarDay("Wednesday"));
 });
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,5 +1,14 @@
 type Size = "small" | "big";
 
-export function order(size: Size) {
-  throw new Error("Not implemented");
+export type WeekDay = "Monday" | "Tuesday" | "Wednesday";
+
+export interface Calendar {
+  day: WeekDay;
+}
+
+type OrderDate = Calendar;
+type DeliveryDate = Calendar;
+
+export function order(size: Size, orderDate: OrderDate): DeliveryDate {
+  return { day: "Wednesday" };
 }
